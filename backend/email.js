bdee661d7cb8196f71ca36a2f307bb63758b50a4
Sriram <bsriramz05@gmail.com>
2025-12-15 16:10:17 +0530

    Change sender email address in email.js

--- backend/email.js
+++ backend/email.js
@@ -52,7 +52,7 @@ async function sendConfirmationEmail(to, data) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      from: "GIT-A-THON <onboarding@resend.dev>",
+      from: "GIT-A-THON <notifications@resend.dev>",
       to,
       subject: "GIT-A-THON 2025 – Registration Confirmed",
       html,
